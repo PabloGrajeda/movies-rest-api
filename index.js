b13063b525fc395a97bfe92deef2a83b5ae5e0d8
Pablo <pablo_082006@hotmail.com>
2021-03-10 21:52:12 -0600

    [Modified]: api route and status responses

--- index.js
+++ index.js
@@ -5,6 +5,6 @@ const PORT = 5000
 const app = express()
 
 app.use(express.json())
-app.use('/movies', moviesRoutes)
+app.use('/api/v1/movies', moviesRoutes)
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
