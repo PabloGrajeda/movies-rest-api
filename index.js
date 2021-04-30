89045812791dabe0bf842499eb3446136c60e5e9
Pablo <pablo_082006@hotmail.com>
2021-04-29 19:24:55 -0600

    [added]: server config

--- index.js
+++ index.js
@@ -9,6 +9,13 @@ dotenv.config()
 
 const app = express()
 
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE, PATCH");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept,access-token, Authorization");
+    next();
+})
+
 app.use(express.json())
 app.use('/api/v1/movies', moviesRoutes)
 app.use('/api/v1/auth', authRoutes)
